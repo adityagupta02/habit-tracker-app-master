4b73ddafae914d27ec40a6ace7270c828877b701
Aditya Bal Gupta <guptaaditya70993@gmail.com>
2023-04-27 01:30:41 +0530

    first commit

---
+++ src/app/authenticated-app/navbar.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { Menu as MenuIcon } from '@material-ui/icons';
+import { useDrawer } from './drawer-context';
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    justifyContent: 'flex-end',
+  },
+  menuButton: {
+    // Push other menu elements to the right
+    marginRight: 'auto',
+    // [theme.breakpoints.up('lg')]: {
+    //   display: 'none',
+    // },
+  },
+}));
+
+function Navbar({ children }) {
+  const classes = useStyles();
+
+  const { onDrawerToggle } = useDrawer();
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={onDrawerToggle}
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        {children}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export { Navbar };
